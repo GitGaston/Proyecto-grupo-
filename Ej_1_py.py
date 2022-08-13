65becac3f499afe19f4b9fca5f4362f2c90858ee
Ernesto C <cevascoagustin@gmail.com>
2022-08-13 01:06:43 -0300

    Ejercicio 1 python

---
+++ Ej_1_py.py
@@ -0,0 +1,8 @@
+Dueno = [28957346, 'Juan', 'Perez', 4789689, 'Belgrano 101']
+dni = 26000000
+print('Ingrese DNI: ')
+input()
+if dni >= 26000000:
+    print('El telefono del dueño es:',Dueno[3])
+else:
+    print()
